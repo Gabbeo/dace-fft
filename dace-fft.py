c3e2f215608975c8e5483390c3e32fe028b6035a
Gabriel Bengtsson <gabriel.jo.bengtsson@gmail.com>
2020-04-21 10:59:40 +0200

    Fix MapReduceFusion problem, y-output was not cleared properly

--- dace-fft.py
+++ dace-fft.py
@@ -10,72 +10,6 @@ N = dace.symbol('N')
 R = dace.symbol('R')
 K = dace.symbol('K')
 
-datatype = dace.complex64
-datatype_np = np.complex64
-dtype = dace.complex64
-
-##############
-#### DACE ####
-##############
-def Permute_tensorprod_I(x, r, k, i):
-    n_p = dace.define_local([1], dtype=dace.int64)
-    I_len = dace.define_local([1], dtype=dace.int64)
-    @dace.tasklet
-    def calc_index():
-        n_p_o >> n_p[0]
-        I_len_o >> I_len[0]
-        n_p_o = r ** (k - i - 1)
-        I_len_o = r ** i
-
-    tmp = dace.define_local([N], dtype=dace.complex64)
-
-    @dace.map(_[0:r, 0:n_p[0], 0:I_len[0]])
-    def permute(ii, jj, kk):
-        x_in << x[I_len * (jj * r + ii) + kk]
-        tmp_out >> tmp[ii * I_len * n_p + jj * I_len + kk]
-
-        tmp_out = x_in
-        
-    @dace.map(_[0:N])
-    def transfer_back(i):
-        tmp_in << tmp[i]
-        x_out >> x[i]
-        x_out = tmp_in
-
-def Generate_twiddles(D, i, r, k):
-    # Twiddles
-    L_q = dace.define_local([1], dtype=dace.int64)
-    Lq_rq = dace.define_local([1], dtype=dace.int64)
-    n_Lq = dace.define_local([1], dtype=dace.int64)
-    
-    @dace.tasklet
-    def calc_indices():
-        L_q_o >> L_q[0]
-        Lq_rq_o >> Lq_rq[0]
-        n_Lq_o >> n_Lq[0]
-        
-        L_q_o = r ** (i + 1)
-        Lq_rq_o = r ** i
-        n_Lq_o = r ** (k - i - 1)
-        
-        
-    @dace.map(_[0:r, 0:Lq_rq[0], 0:n_Lq[0]])
-    def generate_twiddles(ii, jj, kk):
-        twiddle >> D[n_Lq * (ii * Lq_rq + jj) + kk]
-        twiddle = np.exp(-2j * np.pi * ii * jj / L_q)
-        exp(-dace.complex64(0, 2 * 3.14159265359 * ii * jj / L_q))
-
-def A_tensorprod_I(A, x, y, m, n):
-    tmp = dace.define_local([N], dtype=dace.complex64)
-    
-    @dace.map(_[0:n, 0:m, 0:m])
-    def tensormult(ii, jj, kk):
-        A_in << A[jj, kk]
-        x_in << x[ii + n * kk]
-        y_out >> y[ii + n * jj]
-        
-        y_out += A_in * x_in
-
 ############################################################################
 @dace.program(dace.complex64[N], dace.complex64[N])
 def own_fft(x, y):
@@ -110,12 +44,13 @@ def own_fft(x, y):
             n_p_o = R ** i
             I_len_o = R ** (K - i - 1)
     
+        tmp_perm = dace.define_local([N], dtype=dtype)
         @dace.map(_[0:R, 0:n_p[0], 0:I_len[0]])
         def permute(ii, jj, kk):
             I_len_in << I_len[0]
             n_p_in << n_p[0]
             y_in << y[I_len_in * (jj * R + ii) + kk]
-            tmp_out >> tmp[I_len_in * (ii * n_p_in + jj) + kk]
+            tmp_out >> tmp_perm[I_len_in * (ii * n_p_in + jj) + kk]
     
             tmp_out = y_in
             
@@ -148,13 +83,14 @@ def own_fft(x, y):
             
 
         # x = D_op(D, x, n)
+        tmp_twid = dace.define_local([N], dtype=dtype)
         @dace.map(_[0:N])
         def twiddle_multiplication(i):
-            tmp_in << tmp[i]
+            tmp_in << tmp_perm[i]
             D_in << D[i]
-            y_out >> y[i]
+            tmp_out >> tmp_twid[i]
             
-            y_out = tmp_in * D_in
+            tmp_out = tmp_in * D_in
 
         # ---------------------------------------------------------------------
         # Vector DFT multiplication
@@ -171,12 +107,20 @@ def own_fft(x, y):
         def tensormult(ii, jj, kk):
             r_k_m1_in << r_k_m1[0]
             dft_in << dft_mat[jj, kk]
-            y_in << y[ii + r_k_m1_in * kk]
+            tmp_in << tmp_twid[ii + r_k_m1_in * kk]
             tmp_y_out >> tmp_y[ii + r_k_m1_in * jj, ii + r_k_m1_in * kk]
 
-            tmp_y_out = dft_in * y_in
+            tmp_y_out = dft_in * tmp_in
             
-        dace.reduce(lambda a, b: a + b, tmp_y, y, axis=1, identity=0)
+        tmp_red = dace.define_local([N], dtype=dtype)
+        dace.reduce(lambda a, b: a + b, tmp_y, tmp_red, axis=1, identity=0)
+        
+        @dace.map(_[0:N])
+        def move_to_y(i):
+            tmp_in << tmp_red[i]
+            y_out >> y[i]
+            
+            y_out = tmp_in
         
 if __name__ == "__main__":
     print("==== Program start ====")
@@ -184,8 +128,8 @@ if __name__ == "__main__":
     dace.Config.set('profiling', value=True)
     dace.Config.set('treps', value=1)
 
-    r = 4
-    k = 5
+    r = 2
+    k = 2
     n = r ** k
     print('FFT on real vector of length %d' % n)
 
